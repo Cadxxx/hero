58c2effa3fee0554a3097c71c9b7523af4e3b3a4
unknown <up202108714@up.pt>
2022-10-08 14:33:32 +0100

    Finish hero

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,46 +1,77 @@
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class Game {
-    private int x = 10;
-    private int y = 10;
-    private Screen screen;
-    public void main(String[] args) {
+    public Game(){
         try {
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            this.screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null); // we don't need a cursor
-            screen.startScreen(); // screens must be started
-            screen.doResizeIfNecessary(); // resize screen if necessary
-        } catch (IOException e) {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
+
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e){
             e.printStackTrace();
         }
+
+        arena = new Arena(40, 20);
     }
+
     private void draw() throws IOException{
-        getScreen().clear();
-        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
-        getScreen().refresh();
-    }
-    public void run() throws IOException{
-        draw();
-        KeyStroke key = screen.readInput();
+        screen.clear();
+        arena.draw(screen.newTextGraphics());
+        screen.refresh();
     }
 
-    public Screen getScreen(){
-        return screen;
-    }
-
-    public void setScreen(Screen screen) {
-        this.screen = screen;
-    }
-    private void processKey(KeyStroke key) {
+    private void processKey(com.googlecode.lanterna.input.KeyStroke key){
         System.out.println(key);
+        switch (key.getKeyType()) {
+            case ArrowUp    -> arena.moveHero(arena.moveUp());
+            case ArrowDown  -> arena.moveHero(arena.moveDown());
+            case ArrowLeft  -> arena.moveHero(arena.moveLeft());
+            case ArrowRight -> arena.moveHero(arena.moveRight());
+        }
     }
+
+    public void run() {
+        try {
+            while(true) {
+                draw();
+                com.googlecode.lanterna.input.KeyStroke key = screen.readInput();
+                processKey(key);
+                if(arena.verifyMonsterCollisions()){
+                    screen.close();
+                    break;
+                }
+
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == ('q'))
+                    screen.close();
+                if (key.getKeyType() == KeyType.EOF)
+                    break;
+
+                arena.moveMonsters();
+                if(arena.verifyMonsterCollisions()){
+                    screen.close();
+                    break;
+                }
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private Screen screen;
+    private Arena arena;
 }
 
